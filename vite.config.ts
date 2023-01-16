3c945812ee5e5222f888b926bd836a805eb7c94c
ASTERISK Kwon <hello@asterisk.moe>
2023-01-16 09:25:00 +0000

    chore: project setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: {
+      path: 'ws',
+      port: 5173,
+      timeout: 3000,
+      clientPort: 443,
+    },
+    strictPort: true,
+  }
+});
