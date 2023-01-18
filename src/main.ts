07a68c7333ad9312d27e5b8fd01cfa53daf1e597
ASTERISK Kwon <hello@asterisk.moe>
2023-01-18 03:56:11 +0000

    feat: pinia 도입

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { router } from './router'
 import App from './App.vue'
 import '/src/style.css'
 import 'pretendard/dist/web/static/pretendard-dynamic-subset.css';
 
 const app = createApp(App);
-app.use(router);
-app.mount('#app');
+const pinia = createPinia()
 
+app.use(pinia)
+app.use(router);
+
+app.mount('#app');
