ae550f6144d8413e763eea048d7e5da2fee1fab8
ASTERISK Kwon <hello@asterisk.moe>
2023-01-19 11:06:05 +0000

    feat: 패키지 추가

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,11 @@ module.exports = {
   theme: {
     extend: {},
   },
+  corePlugins: {
+    aspectRatio: false,
+  },
   plugins: [
     require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
   ],
 }
