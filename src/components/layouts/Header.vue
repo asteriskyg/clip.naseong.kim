e84a47fe34306d2f8742af4c54935d5a8d090fc2
ASTERISK Kwon <hello@asterisk.moe>
2023-01-23 09:22:04 +0000

    feat: 공지사항 변경

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -141,9 +141,9 @@
     </div>
     <div class="flex flex-col items-center gap-2 border-t bg-slate-50/30 p-3">
       <a
-        href="https://tgd.kr/s/naseongkim/68400017"
+        href="https://tgd.kr/s/naseongkim/68444665"
         class="border-b border-blue-500 text-blue-500 transition-all duration-300 hover:text-blue-800"
-        >업데이트: 프로필 기능 추가</a
+        >업데이트: 클립 수정 기능 추가</a
       >
       <div class="flex flex-wrap gap-1 text-sm">
         <div>베타 테스트 중 입니다.</div>
