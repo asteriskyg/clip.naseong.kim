cabb1cd8971697a67cbfe7cb7fb20d668b3a5f8d
ASTERISK Kwon <hello@asterisk.moe>
2023-02-14 11:15:28 +0000

    feat: pathname에 맞게 레이아웃 바뀌도록 변경

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,28 @@
 import Header from './components/layouts/HeaderLayout.vue';
 import Footer from './components/layouts/FooterLayout.vue';
 import './components/ChannelTalk.js';
+import { computed } from 'vue';
+
+const isExtension = computed(() => {
+  if (window.location.pathname === '/extension') {
+    return true;
+  } else {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // eslint-disable-next-line no-undef
+    ChannelIO('boot', {
+      pluginKey: '02450e74-074b-4597-bb95-a9aa42f7f5d2',
+    });
+    return false;
+  }
+});
 </script>
 <template>
   <div class="min-h-full min-w-full">
-    <Header />
-    <router-view class="h-full" />
-    <Footer />
+    <Header :class="{'hidden': isExtension}" />
+    <div class="h-full">
+      <router-view />
+    </div>
+    <Footer :class="{'hidden': isExtension}" />
   </div>
 </template>
