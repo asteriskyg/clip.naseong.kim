d57927546ed1beb4bda4f7238cbca601c4053a99
ASTERISK Kwon <hello@asterisk.moe>
2023-04-17 11:29:05 +0000

    feat: RecentClipList 컴포넌트 사용하도록 변경

--- src/common/userProfile.vue
+++ src/common/userProfile.vue
@@ -115,59 +115,10 @@
         </div>
       </div>
     </div>
-    <div class="p-6 md:p-0">
-      <div class="mb-4 text-2xl font-bold">
-        {{ user?.displayName }}님의 클립
-      </div>
-      <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        <a
-          v-for="item in clips"
-          :key="item.clipName"
-          :href="`/detail/${item.clipName}`"
-          class="min-w-fit overflow-hidden rounded-lg border bg-white transition-shadow duration-300 hover:shadow-2xl"
-        >
-          <div class="relative aspect-w-16 aspect-h-9">
-            <div>
-              <img
-                :src="`https://customer-lsoi5zwkd51of53g.cloudflarestream.com/${item.contentId}/thumbnails/thumbnail.jpg`"
-                alt=""
-              >
-              <div
-                class="absolute bottom-2 right-2 flex h-7 w-12 items-center justify-center rounded-lg bg-black/20 text-sm text-white backdrop-blur-sm"
-              >
-                {{
-                  item.clipDuration > 59
-                    ? `1:${item.clipDuration - 60}`
-                    : `0:${item.clipDuration}`
-                }}
-              </div>
-            </div>
-          </div>
-          <div class="p-4">
-            <div class="text-xl font-semibold line-clamp-1">
-              {{ item.contentName }}
-            </div>
-            <div class="mb-3">
-              {{ item.gameName }}
-            </div>
-            <div style="overflow-wrap: anywhere">
-              {{ dayjs().locale("ko").to(dayjs(item.clipCreatedAt)) }}
-            </div>
-          </div>
-        </a>
-      </div>
-      <div
-        v-if="clips.length - clipIndex * 12 > 12"
-        class="mt-6 flex w-full items-center justify-center"
-      >
-        <button
-          class="rounded-full bg-gray-200 px-6 py-3 transition-colors duration-300 hover:text-white active:bg-blue-500 md:hover:bg-blue-500"
-          @click="loadMore"
-        >
-          더보기
-        </button>
-      </div>
-    </div>
+    <RecentClipList
+      :title="`${ user?.displayName }님의 클립`"
+      :get="'user'"
+    />
   </div>
 </template>
 <script setup lang="ts">
@@ -193,22 +144,6 @@ interface User {
   subscription: number | undefined;
 }
 
-interface Clips {
-  clipCreatedAt: Date;
-  clipDuration: number;
-  clipLastEdited: Date;
-  contentId: string;
-  contentName: string;
-  creatorId: number;
-  creatorName: string;
-  gameId: number;
-  gameName: string;
-  streamStartedAt: Date;
-  clipName: string;
-  __v: number;
-  _id: string;
-}
-
 const VITE_API_URL: string = import.meta.env.VITE_API_URL;
 
 const auth = useAuth();
@@ -217,8 +152,6 @@ const route = useRoute();
 const me = ref<User>();
 const user = ref<User>();
 const profileBgInput = ref<HTMLInputElement>();
-const clips = ref<Clips[]>([]);
-const clipIndex = ref(1);
 const isOpen = ref(false);
 
 function setIsOpen(value: boolean) {
@@ -234,14 +167,6 @@ onMounted(async () => {
     },
   });
   user.value = userData.data;
-
-  const clipsData = await axios.get(`${VITE_API_URL}/getUserClips?offset=0`, {
-    headers: {
-      id: route.params.id,
-    },
-  });
-
-  clips.value = clipsData.data;
 });
 
 const tier = computed((): string | undefined => {
@@ -255,17 +180,6 @@ const tier = computed((): string | undefined => {
     return undefined;
   }
 });
-
-async function loadMore() {
-  const clipLists = await axios.get(
-    `${VITE_API_URL}/getClipLists?offset=${clipIndex.value}`,
-  );
-  clipIndex.value = clipIndex.value + 1;
-
-  for (let i = 0; i < clipLists.data.length; i++) {
-    clips.value.push(clipLists.data[i]);
-  }
-}
 
 async function uploadBg() {
   if (!profileBgInput.value?.files || !user.value) {
