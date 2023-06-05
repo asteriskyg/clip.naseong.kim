7b8207e6260a36bb6b164e47be7ba0c088c294d2
ASTERISK Kwon <hello@asterisk.moe>
2023-06-05 06:26:12 +0000

    feat: 다크모드 추가

---
+++ src/components/ToggleDark.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { UseDark } from '@vueuse/components';
+</script>
+<template>
+  <UseDark v-slot="{ isDark, toggleDark }">
+    <button @click="toggleDark()">
+      <i
+        class="xi-x text-black  dark:text-slate-200 transition-all duration-300 ease-in-out"
+        :class="{
+          'xi-sun-o': isDark,
+          'xi-night': !isDark,
+        }"
+      />
+    </button>
+  </UseDark>
+</template>
